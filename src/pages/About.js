2f695f7550ed5cbee75f8110a5e602c79ff0c36e
arnavnarain <narain.a@northeastern.edu>
2023-04-08 22:28:10 -0400

    cleanup

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,12 @@
 const About = () => { 
     
     return (
-        <p> Test </p>
+        <center>
+            <p>
+                Create by Arnav Narain and Srikar Iragavarapu. 
+                For any questions or concerns, please call: 502-819-8094
+            </p>
+        </center>
     )
 }
 
