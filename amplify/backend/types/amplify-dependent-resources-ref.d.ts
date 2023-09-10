3d429908fc4896eb076c5eb33e198ffc5a421109
arnavnarain <narain.a@northeastern.edu>
2023-09-10 00:29:31 -0400

    Live Ranking serverless function

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -23,6 +23,13 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
+    "LiveRanking": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "OAuthCallback": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
